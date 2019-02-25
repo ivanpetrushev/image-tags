23b69dcd3d38c7116db125245000ea37423d2b5b
Ivan Petrushev <ivanatora@gmail.com>
2019-02-25 21:04:09 +0200

    initial models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+
+class File(models.Model):
+    filename = models.CharField(max_length=255)
+    last_viewed = models.DateTimeField()
+    needs_tagging = models.BooleanField(default=False)
+    cnt_views = models.IntegerField()
+    created = models.DateTimeField()
+
+    class Meta:
+        db_table = 'files'
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+    navigation = models.CharField(max_length=1)
+
+    class Meta:
+        db_table = 'tags'
+
+
+class FileTag(models.Model):
+    file = models.ForeignKey(File, on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'files_tags'
