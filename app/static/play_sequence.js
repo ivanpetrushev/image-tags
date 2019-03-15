50ed56be00beaa345361b3802948a4f3058e318b
Ivan Petrushev <ivanatora@gmail.com>
2019-03-15 10:06:42 +0200

    fixing image resize

--- app/static/play_sequence.js
+++ app/static/play_sequence.js
@@ -3,6 +3,7 @@ if (typeof(aSequenceContents) != 'undefined') {
     var iCurrentFileId;
     var oInterval;
     var iMsInterval = 3000;
+    var bRunning = true;
 
     function writeTitle(sMsg){
         if (typeof sMsg == 'undefined'){
@@ -31,7 +32,13 @@ if (typeof(aSequenceContents) != 'undefined') {
         writeTitle();
     }
 
-    function loadNextImage(){
+    function loadNextImage(force){
+        if (typeof force == 'undefined') {
+            force = false;
+        }
+        if (! bRunning && ! force) {
+            return;
+        }
         for (var i = 0; i < aSequenceContents.length-1; i++){
             if (aSequenceContents[i] == iCurrentFileId){
                 loadImage(aSequenceContents[i+1]);
@@ -61,7 +68,7 @@ if (typeof(aSequenceContents) != 'undefined') {
             }
             if (iCode == 39){ // RIGHT ARROW
                 clearInterval(oInterval);
-                loadNextImage();
+                loadNextImage(true);
                 oInterval = setInterval("loadNextImage()", iMsInterval);
             }
             if (iCode == 37){ // LEFT ARROW
@@ -71,32 +78,24 @@ if (typeof(aSequenceContents) != 'undefined') {
             }
         })
 
-        // преоразмеряване на картинката по време на зареждането ѝ
+        // autoscale image at load time
         $("#image_here").load(function(){
             $(this).removeAttr('width');
             $(this).removeAttr('height');
 
-            var iWidth = $("#image_here").width();
-            var iHeight = $("#image_here").height();
-            var fRatio = parseFloat(iWidth / iHeight);
-
             var iMaxWidth = window.innerWidth - 10;
             var iMaxHeight = window.innerHeight - 100;
 
-            var iDeltaWidth = iWidth - iMaxWidth;
-            var iDeltaHeight = iHeight - iMaxHeight;
+            // get image original dimensions
+            var iWidth = $("#image_here")[0].naturalWidth;
+            var iHeight = $("#image_here")[0].naturalHeight;
 
-            if (iDeltaWidth > iDeltaHeight){
-                $(this).attr('width', iMaxWidth);
-                var fCorrection = iWidth / iMaxWidth;
-                $(this).attr('height', iHeight / fCorrection);
-            }
-            else {
-                $(this).attr('height', iMaxHeight);
-                var fCorrection = iHeight / iMaxHeight;
-                $(this).attr('width', iWidth / fCorrection)
-            }
+            var fRatio = Math.min(iMaxWidth / iWidth, iMaxHeight / iHeight);
+            var iNewWidth = iWidth * fRatio;
+            var iNewHeight = iHeight * fRatio;
 
+            $(this).width(iNewWidth + 'px');
+            $(this).height(iNewHeight + 'px');
         });
 
         loadImage(aSequenceContents[0]);
